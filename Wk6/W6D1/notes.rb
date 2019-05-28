bee6507441e567211e9151ada9fc3d3e601cc686
paulkwchoi92 <paulkwchoi92@gmail.com>
2019-05-28 08:33:57 -0700

    study hall notes

---
+++ Wk6/W6D1/notes.rb
@@ -0,0 +1,51 @@
+# ADT
+
+# set
+  #set
+  #delete
+     # no duplicates
+#map/dictionary structure
+  #set   k = v
+  #get  get k
+  #delete   delete k
+    # no duplicates only one key
+
+# why ADT is important
+  # different algorithms are better implemented to different data structures
+
+# STACK
+  #LASTINFIRST OUT
+  # push & pop
+    # push puts data into array from the back
+    # pop taking out data out of array
+    #often done with recursion
+
+# QUEUE
+  #FIRST IN FIRST OUT
+  # push and shift
+  # push entry from the back
+  #shift take out entry from the front
+  # or unshift and pop
+
+
+
+#TREE
+  #    0        #binary tree
+  #  0   0
+  #0   0 0 0
+
+   #           0              Ternary tree
+   #     0     0      0
+   #  0   0 0 00 0  0 0 0
+
+   # tree with undefinite amout of childre = n-ary tree or poly tree
+
+ #root of tree is the first element of a tree
+ #nodes are each level of tree
+
+ #leafnode = last node of a tree
+ # DFS
+   #straight down the tree
+
+# BFS
+   # one layer at a time
